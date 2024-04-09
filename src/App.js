35126cb0880102cde09a7f1a3c657d66c74939c0
Rahul John <msdrahuljohn@gmail.com>
2024-04-09 16:07:25 +0530

    imported react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
-
+import react from 'react'
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
